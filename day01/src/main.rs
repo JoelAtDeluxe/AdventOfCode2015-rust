962e39e4093174a94f56e92c350a784b3f69ac63
Joel Smith <Hikash@gmail.com>
2019-07-31 11:42:33 -0700

    day 1 + 2 solutions

---
+++ day01/src/main.rs
@@ -0,0 +1,71 @@
+fn main() {
+    let path = "input.txt";
+    let input = std::fs::read_to_string(path).expect("Failed to open: input.txt");
+    let (last_floor, basement_step) = analyze_input(&input);
+
+    println!("With input: {}", &input[..]);
+    println!("Last floor reached: {}", last_floor);
+    match basement_step {
+        Some(val) => println!("Step on which basement is reached (first time): {}", val),
+        None => println!("Never reached the basement"),
+    }
+}
+
+// not really needed unless doing some parsing
+// fn read_file(path: &str) -> String {
+//     let result = std::fs::read_to_string(path);
+//     if result.is_err() {
+//         panic!("Error trying to read file: {} ;; err text: {:?}", path, result.unwrap_err());
+//     }
+//     let contents = result.unwrap();
+//     contents
+// }
+
+fn analyze_input(s: &str) -> (i32, Option<usize>) {
+    let mut count = 0;
+    let mut basement_step: Option<usize> = None;
+
+    for (i, byte) in s.chars().enumerate() {
+        match byte {
+            '(' => count += 1,
+            ')' => {
+                count -= 1;
+                if count < 0 && basement_step == None {
+                    basement_step = Some(i + 1);
+                }
+            }
+            _ => (),
+        };
+    }
+    (count, basement_step)
+}
+
+// fn count_floors(s: &str) -> i32 {
+//     let mut count = 0;
+//     for byte in s.chars() {
+//         match byte {
+//             '(' => count += 1,
+//             ')' => count -= 1,
+//             _ => (),
+//         };
+//     }
+//     count
+// }
+
+// fn first_basement(s: &str) -> Option<usize> {
+//     let mut count = 0;
+//     for (i, byte) in s.chars().enumerate() {
+//         match byte {
+//             '(' => count += 1,
+//             ')' => {
+//                 count -= 1;
+//                 println!("step: {} => count: {}", i + 1, count);
+//                 if count < 0 {
+//                     return Some(i + 1);
+//                 }
+//             }
+//             _ => (),
+//         }
+//     }
+//     return None;
+// }
---
+++ day02/src/main.rs
@@ -0,0 +1,74 @@
+use std::cmp;
+
+fn main() {
+    let gifts = parse_input("input.txt");
+
+    let total_paper_needed: u32 = (&gifts).into_iter()
+        .map(|gift| gift.paper_required())
+        .sum();
+
+    println!("Total amount of paper needed: {}", total_paper_needed);
+    
+    let total_ribbon_needed: u32 = gifts.into_iter()
+        .map(|gift| gift.ribbon_required())
+        .sum();
+
+    println!("Total amount of ribbon needed: {}", total_ribbon_needed);
+
+    //this should result in, probably, 1586300
+}
+
+struct Gift {
+    length: u32,
+    width: u32,
+    height: u32
+}
+
+impl Gift {
+    pub fn new (s: &str) -> Result<Gift, String>{
+        let parts: Vec<&str> = s.split("x").collect();
+
+        match parts.len() {
+            3 => {
+                let result: Result<Vec<u32>, _> = parts.into_iter().map(|x| x.parse::<u32>() ).collect();
+                match result {
+                    Ok(lwh) => Ok(Gift {length: lwh[0], width: lwh[1], height: lwh[2]}),
+                    Err(e) => Err( format!("Could not parse values. Error: {}", e)),
+                }
+            },
+            _ => Err( format!("Input didn't match expected form: {}", s)),
+        }        
+    }
+
+    pub fn paper_required(&self) -> u32 {
+        let surface_area = 2 * ((self.length * self.height) + (self.width * self.height) + (self.length * self.width));
+        let min_area = (self.length * self.width * self.height) / cmp::max(self.height, cmp::max(self.length, self.width));
+        surface_area + min_area
+    }
+
+    pub fn ribbon_required(&self) -> u32 {
+        self.volume() + self.shortest_perimeter()
+    }
+
+    fn shortest_perimeter(&self) -> u32 {
+        let total = self.length + self.width + self.height;
+        let smallest_half_parimeter = total - cmp::max(self.length, cmp::max(self.width, self.height));
+        
+        smallest_half_parimeter * 2
+    }
+
+    fn volume(&self) -> u32 {
+        self.length * self.width * self.height
+    }
+}
+
+fn parse_input(path: &str) -> Vec<Gift> {
+    let input = std::fs::read_to_string(path).expect("Failed to open: input.txt");
+    let mut gifts = Vec::new();
+
+    for line in input.split("\n") {
+        let gift = Gift::new(line);
+        gifts.push(gift.unwrap_or_else(|err| panic!("Could not construct a gift: {}", err) ));
+    }
+    gifts
+}
